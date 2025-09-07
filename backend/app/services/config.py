b6124e34773a444f79244676e0e5398a7fb4146a
ClaudiaYao <claudia.yao2012@gmail.com>
2025-09-07 12:48:50 +0800

    update calling to .env file

--- backend/app/services/config.py
+++ backend/app/services/config.py
@@ -40,8 +40,6 @@ LLM_MODEL_KEY = os.getenv("LLM_MODEL_KEY")
 USE_POSTGRESQL=os.getenv("USE_POSTGRESQL")
 
 API_KEY = os.getenv("API_KEY")
-RUNPOD_URL = "http://0.0.0.0:8000"
-# RUNPOD_URL = "https://5n8f9dz09pal1t-8000.proxy.runpod.net"
 REDIS_HOST=os.getenv("REDIS_HOST")
 FRONTEND_URL_S3=os.getenv("FRONTEND_URL_S3")
 RUNPOD_URL=os.getenv("RUNPOD_URL", "")
